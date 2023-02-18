64ec16b04a18a33ed92d6ac597afe52604c84492
Harrier55 <harrier55@yandex.ru>
2023-02-18 16:23:00 +0500

    Переход на ветку Develop

--- app/src/main/java/com/example/git_followers/presentation/MainActivity.kt
+++ app/src/main/java/com/example/git_followers/presentation/MainActivity.kt
@@ -23,22 +23,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Git_followersTheme {
-        Greeting("Android")
-    }
+fun MainScreen() {
+    Text(text = "Hello ")
 }
+
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Git_followersTheme {
+//
+//    }
+//}
